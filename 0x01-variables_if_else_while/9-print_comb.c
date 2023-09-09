e9d8caea52a0b74c41b7e1f9947c566cfb7467ea
Ordu <ordudesigns@gmail.com>
2023-09-08 17:09:41 -0700

    variables if

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,24 +1,23 @@
 #include <stdio.h>
 
-/**
+/*
  * main - the main function
  *
  * Return: always (0)
  */
-
 int main(void)
 {
 	int i;
-	char d;
 
-	for (i = '0'; i <= '9'; i++)
+	for (i = 0; i < 10; i++)
 	{
-		putchar(i);
+		putchar(i + '0');
+		if (i != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
-	for (d = 'a'; d <= 'f'; d++)
-	{
-		putchar(d);
-	}
-	putchar('\n');
+	putchar ('\n');
 	return (0);
 }
