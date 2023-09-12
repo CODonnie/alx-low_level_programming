eb80601e20394f14cea9a552f237b195baaadb6c
Ordu <ordudesigns@gmail.com>
2023-09-12 06:30:09 -0700

    10x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - main function
+ *
+ * Return: always (0)
+ */
+void print_alphabet_x10(void)
+{
+	char x;
+	int y;
+
+	for (y = 0;  y <= 10; y++)
+	{
+		for (x = 'a'; x <= 'z'; x++)
+		{
+			_putchar(x);
+		}
+		_putchar('\n');
+	}
+}
