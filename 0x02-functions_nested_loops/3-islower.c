60dd7f6125a915cb87bed29ebb8c7cf7fb9db290
Ordu <ordudesigns@gmail.com>
2023-09-12 07:18:51 -0700

    lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _islower - the main function
+ *
+ * Return: 1 if c is lowercase or 0 for anything else
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	return (0);
+}
